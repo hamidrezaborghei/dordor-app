6d421c62b3247c9f3ebe0b355d2e6c2b76b4040d
oonjib <backdev.oonjib@gmail.com>
2024-06-17 14:49:36 +0330

    fix not used smth

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,9 +1,4 @@
-import { Section, Cell, Image, List } from "@telegram-apps/telegram-ui";
 import type { FC } from "react";
-
-import { Link } from "@/components/Link/Link.tsx";
-
-import tonSvg from "./ton.svg";
 
 import "./IndexPage.css";
 import { useInitData } from "@tma.js/sdk-react";
